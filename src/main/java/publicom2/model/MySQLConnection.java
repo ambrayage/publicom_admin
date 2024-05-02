264bc1e6d39c1c6ac6aae8c47a03e9a766dd42c3
ambrayage <ambre.ramos.pro@gmail.com>
2024-05-02 12:08:48 +0200

    connexionnnnn

--- src/main/java/publicom2/model/MySQLConnection.java
+++ src/main/java/publicom2/model/MySQLConnection.java
@@ -16,9 +16,9 @@ import java.util.logging.Logger;
  */
 public class MySQLConnection {
     
-    private static final String url = "jdbc:mysql://localhost:3306/mlr_publicom";
-    private static final String user = "root";
-    private static final String pass = "";
+    private static final String url = "jdbc:mysql://172.28.36.13:3306/mlr_publicom";
+    private static final String user = "aramos";
+    private static final String pass = "aramos";
     
     private static Connection con = null;
     
